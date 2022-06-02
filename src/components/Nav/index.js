34ca3bb5ed5d7494c974260ce88248fd6743130f
dvasquez4155 <main@dvasquez4155.com>
2022-06-01 22:37:56 -0700

    Added a nav bar

---
+++ src/components/Nav/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+
+require('./index.css')
+
+function Nav(props) {
+    return (
+        <main>
+        <nav className="navbar navbar-dark navbar-expand-lg bg-dark ">
+            <div className="container">
+                <a className="navbar-brand logo" href="/">
+                Training{" "}
+                </a>
+                
+                <div id="navbarNav">
+                <ul id="links" className="nav  ml-auto">
+                    {/* <li className="nav-item" role="presentation">
+                    <Button variant="outline-*" className="nav-link" href="/login">
+                        Join Session
+                    </Button>
+                    </li> */}
+                    <li className="mr-3 nav-item" role="presentation">
+                    <Button variant="outline-*" className="nav-link" href="/">
+                        Home
+                    </Button>
+                    </li>
+                </ul>
+                </div>
+            </div>
+            </nav>
+        </main>
+    );
+}
+
+export default Nav;
