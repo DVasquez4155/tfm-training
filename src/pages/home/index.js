b34b5c3dfc9e67bb67cacf2c35a5c804397a91af
dvasquez4155 <main@dvasquez4155.com>
2022-06-03 08:13:53 -0700

    Outlet was unnecessary

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { CardGroup } from "react-bootstrap";
-import { Link, Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 function Nav(props) {
