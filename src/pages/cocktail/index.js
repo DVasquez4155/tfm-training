defb23a2cbf8814a65fd60fa885100a887327d69
dvasquez4155 <main@dvasquez4155.com>
2022-06-03 10:12:14 -0700

    Reverted

--- src/pages/cocktail/index.js
+++ src/pages/cocktail/index.js
@@ -1,19 +1,32 @@
 import React from "react";
-import { Card, ListGroup } from "react-bootstrap";
+import { Card, Form, ListGroup } from "react-bootstrap";
 // import Button from 'react-bootstrap/Button';
 import specs from '../../db/cocktails.json';
 
 class index extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: 'Cocktails'};
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
     render() {
         return (
             <>
+                <Form.Select aria-label="Default select example" onChange={this.handleChange}>
+                    {Object.keys(specs).map((desc, descKey) => {
+                        return(
+                            <option key={descKey} value={desc}>{desc}</option>
+                        )
+                        
+                    })}
+                </Form.Select>
                 <br />
-                <h1 className="App-header">
-                    Cocktails
-                </h1>
-                <hr />
                 <div className="row">
-                    {specs.map((data, key) => {
+                    {specs[this.state.value]?.map((data, key) => {
                         return (
                             <div className="col-sm-4" key={key}>
                             <Card className="m-1" bg={"Light"}>
